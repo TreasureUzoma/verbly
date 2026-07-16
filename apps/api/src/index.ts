476782c76f913e6d41525838ce8729156ae3c11c
Treasure Uzoma <treasureuzoma650@gmail.com>
2026-07-16 18:49:48 +0100

    build: Configure API for Vercel deployment

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -60,12 +60,17 @@ api.get("/session", (c: Context) => {
 
 app.route("/", api)
 
-serve(
-  {
-    fetch: app.fetch,
-    port: 5000,
-  },
-  (info) => {
-    console.log(`Server is running on http://localhost:${info.port}`)
-  }
-)
+export default app
+
+if (!process.env.VERCEL) {
+  serve(
+    {
+      fetch: app.fetch,
+      port: 5000,
+    },
+    (info) => {
+      console.log(`Server is running on http://localhost:${info.port}`)
+    }
+  )
+}
+
