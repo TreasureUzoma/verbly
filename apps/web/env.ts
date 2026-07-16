018860c84a9432ee66eca479a58ad9ad0f71fb5b
Treasure Uzoma <treasureuzoma650@gmail.com>
2026-07-16 19:36:25 +0100

    chore: Clean up envSchema definition in web app

--- apps/web/env.ts
+++ apps/web/env.ts
@@ -1,6 +1,4 @@
 import { z } from "zod"
-export const envSchema = z.object({
-  // add client-accessible env vars here
-})
+export const envSchema = z.object({})
 
 export const env = envSchema.parse(process.env)
