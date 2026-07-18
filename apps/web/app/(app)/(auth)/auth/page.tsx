8337badeb97f3059f0c37576b20ff27310895890
Treasure Uzoma <treasureuzoma650@gmail.com>
2026-07-18 17:28:01 +0100

    refactor: Client-side Google auth navigation

--- apps/web/app/(app)/(auth)/auth/page.tsx
+++ apps/web/app/(app)/(auth)/auth/page.tsx
@@ -1,9 +1,17 @@
+"use client"
+
 import Image from "next/image"
 
 import { Button } from "@workspace/ui/components/button"
 import { login } from "./actions"
+import { useRouter } from "next/navigation"
 
-export default async function LoginPage() {
+export default function LoginPage() {
+  const router = useRouter()
+  const handleGoogleAuth = async () => {
+    const url = await login()
+    router.push(url)
+  }
   return (
     <div className="flex min-h-screen items-center justify-center px-6">
       <div className="w-full max-w-sm space-y-8 text-center">
@@ -14,8 +22,13 @@ export default async function LoginPage() {
           </p>
         </div>
 
-        <form action={login} className="space-y-4">
-          <Button size="lg" type="submit" className="w-full">
+        <div className="space-y-4">
+          <Button
+            size="lg"
+            type="submit"
+            onClick={() => handleGoogleAuth()}
+            className="w-full"
+          >
             Sign in with Google
             <Image
               src="/assets/logos/google-g.png"
@@ -25,7 +38,7 @@ export default async function LoginPage() {
               className="ml-2"
             />
           </Button>
-        </form>
+        </div>
 
         <p className="text-xs text-neutral-500 dark:text-neutral-500">
           Don&apos;t have an account yet? No problem! We&apos;ll create one for
